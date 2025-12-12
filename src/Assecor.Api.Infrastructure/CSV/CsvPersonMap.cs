c29672377734f8f73c0adab0fba9d6dd3b8aac63
Harms, Rudolph <Rudolph.Harms@value-ag.de>
2025-12-12 08:19:51 +0100

    feat: bootstrapping startup and add swagger

---
+++ src/Assecor.Api.Infrastructure/CSV/CsvPersonMap.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration;
+
+namespace Assecor.Api.Infrastructure.CSV;
+
+public sealed class CsvPersonMap : ClassMap<CsvPerson>
+{
+    public CsvPersonMap()
+    {
+        Map(static m => m.LastName).Index(0).Default(string.Empty);
+        Map(static m => m.FirstName).Index(1).Default(string.Empty);
+        Map(static m => m.Address).Index(2).Optional();
+        Map(static m => m.ColorId).Index(3).Optional();
+    }
+}
--- src/Assecor.Api.Person/Program.cs
+++ src/Assecor.Api.Person/Program.cs
@@ -2,20 +2,22 @@ using Assecor.Api.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Add Infrastructure with Options pattern
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/openapi/v1.json", "v1");
+        }
+    );
 }
 
 app.UseHttpsRedirection();
